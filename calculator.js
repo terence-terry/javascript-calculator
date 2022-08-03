5b77e58b9b691b903965c1977866717af9b26227
javacript-calculator <nforterence2018@gmail.com>
2022-08-03 01:38:37 -0700

    my new calculator

---
+++ calculator.js
@@ -0,0 +1,25 @@
+let result=document.getElementById("inputext");
+
+
+let calculate=(number)=>{
+    result.value+=number;
+}
+let Result=()=>{
+    try{
+
+        result.value=eval(result.value)
+    }
+    catch(err){
+
+        alert("ERRO")
+    }
+}
+function clr(){
+
+    result.value= "";
+}
+
+function del(){
+
+    result.value=result.value.slice(0,-1)
+}
